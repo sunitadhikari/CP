449f5f9712a6471f68b8c7ac7dc6daad4377a82c
sunitadhikari <sunitaadhikari2001@gmail.com>
2024-05-15 22:52:41 +0545

    login component

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,9 @@ export class UserService {
   constructor(private http:HttpClient) { }
   apiUrl:string = environment.api_url
 
-  private registerApi  = environment.api_url
+  private registerApi  = environment.api_url + "userSignup"
+
+  postSignup(data:any):Observable <any>{
+    return this.http.post(this.registerApi, data);
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../core/service/user.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  constructor(private formBuilder:FormBuilder, private router:Router, private userService: UserService){
+  }
+  signupForm ! : FormGroup
+ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
 
+    })
+}
 }
