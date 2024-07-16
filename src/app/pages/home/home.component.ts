560c0793b2f01dab9bf9dee08a88c3e1a0b308c9
sunitadhikari <sunitaadhikari2001@gmail.com>
2024-07-16 12:25:34 +0545

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,11 +14,14 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 })
 export class HomeComponent {
   features = [
-    { title: 'Patient Management', description: 'Manage patient information efficiently.', logo: 'assets/patient-management.png' },
-    { title: 'Doctor Scheduling', description: 'Automate doctor appointments.', logo: 'assets/doctor-scheduling.png' },
-    { title: 'Billing System', description: 'Manage patient invoices and payments.', logo: 'assets/billing-system.png' },
-    { title: 'Inventory Management', description: 'Track hospital inventory.', logo: 'assets/inventory-management.png' },
-    { title: 'Reports & Analytics', description: 'Generate comprehensive reports.', logo: 'assets/reports-analytics.png' },
-    { title: 'Emergency Services', description: 'Streamline emergency care processes.', logo: 'assets/emergency-services.png' }
+    { title: 'APPOINTMENT MANAGEMENT', description: 'Allow patients to schedule and fix appointments with the available doctors with a single click.', icon: 'fas fa-calendar-alt' },
+    { title: 'DOCTOR RECORDS', description: 'Make a complete profile of doctors and manage their availability and schedule their meetings.', icon: ' fas fa-user-md' },
+    { title: 'IN-PATIENT MANAGEMENT', description: 'Manage records of in-patients along with their admission details, bed number, diet, payments and case scenario.', icon: 'fas fa-file-invoice-dollar' },
+    { title: 'OUTPATIENT MANAGEMENT', description: 'Manage payments, services received, medicines and lab reports of patients who are not admitted to the hospital.', icon: 'fas fa-pills' },
+    { title: 'BILLINGS', description: 'Manage all transactions related to payments and invoices.', icon: 'fas fa-chart-line' },
+    { title: 'BED MANAGEMENT SYSTEM', description: 'Allocate and ensure a unique bed number for each patient.', icon: 'fas fa-bed' },
+    { title: 'LABORATORY MANAGEMENT', description: 'Keep detailed records of the tests performed on each patient.', icon: 'fas fa-flask' },
+    { title: 'PRESCRIPTION SYSTEM', description: 'Doctors give prescription to patients.', icon: 'fas fa-prescription-bottle-alt' },
+    { title: 'Emergency Services', description: 'Streamline emergency care processes.', icon: 'fas fa-ambulance' }
   ];
 }
