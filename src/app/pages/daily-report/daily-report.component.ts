bd221341440c0b802d80ed8d5d5263f8d7a964db
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-07-18 16:04:30 +0545

    dailyreport

--- src/app/pages/daily-report/daily-report.component.ts
+++ src/app/pages/daily-report/daily-report.component.ts
@@ -1,34 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { PatientService } from '../../core/service/patient/patient.service';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+interface AdmittedPatient {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  dob: Date;
+  gender: string;
+  contactNumber: string;
+  address: string;
+  medicalHistory: string;
+  department: string;
+  bedNumber: string;
+  admittedAt: Date;
+  isActive: boolean;
+}
 @Component({
   selector: 'app-daily-report',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './daily-report.component.html',
   styleUrl: './daily-report.component.css'
 })
-export class DailyReportComponent {
+export class DailyReportComponent implements OnInit {
   reportForm!: FormGroup;
+  admitPatient: AdmittedPatient[] = [];
+  // admitPatient: [] = [];
+  selectedPatient: any;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.reportForm = this.fb.group({
-      patientName: ['', Validators.required],
-      date: [new Date(), Validators.required],
+      patient: ['', Validators.required],
+      date: ['', Validators.required],
       symptoms: ['', Validators.required],
       diagnosis: ['', Validators.required],
       treatment: ['', Validators.required]
     });
-  }
 
+    this.fetchAdmittedPatients();
+  }
+  fetchAdmittedPatients(): void {
+    const apiUrl = 'http://localhost:3000/admittedpatientbyDepartment';
+    this.http.get<{ patient: AdmittedPatient[] }>(apiUrl).subscribe(
+      (response) => {
+        this.admitPatient = response.patient;
+      },
+      (error) => {
+        console.error('Error fetching admitted patients:', error);
+      }
+    );
+  }
   onSubmit() {
     if (this.reportForm.valid) {
       console.log(this.reportForm.value);
-      this.reportForm.reset();
-    } else {
-      console.error('Form validation failed');
+      // Handle form submission logic here
     }
   }
 }
